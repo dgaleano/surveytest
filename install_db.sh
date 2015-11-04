7641f1ea4b7edf5190cf52393b9cd757b088f02b
diego <dmgp.91@gmail.com>
2015-11-04 15:18:41 -0600

    including script install db

---
+++ install_db.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+echo "Please write the user of mysql:"
+read user_db
+echo "Please write the pass of mysql:"
+read -s password_mysql
+echo
+mysql -u $user_db -p$password_mysql -e "create database survey"
+mysql -u $user_db -p$password_mysql survey < survey.sql
+cat < .env.php <<EOF
+<?php
+return array(
+    'DBhost' => 'localhost',
+    'DBname' => 'survey',
+    'DBuser' => $user_db,
+    'DBpass' => $password_mysql,
+);
+?>
+EOF
+exit
+
